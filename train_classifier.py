ff40736715a28778428914d29afbad7252750930
Jo Walsh <jowalsh@bgs.ac.uk>
2018-10-07 08:39:08 +0100

    try adding a tensorboard logger to the training routine, havent tried loading the logs though and likely enough we are logging gobbledegook

--- train_classifier.py
+++ train_classifier.py
@@ -10,13 +10,14 @@ import logging
 import torch
 import torch.nn as nn
 #from VGG import vgg_custom
-#from SimpleCNN import simple_cnn
 import torch.optim as optim
 from torch.optim import lr_scheduler
 import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
+
 from prepare_image_model import create_dataloader, create_imagefolder
+from logger import Logger
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -24,6 +25,7 @@ EPOCHS=100
 LEARN_RATE=0.001
 MOMENTUM=0.9
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+LOGGER=Logger('./logs')
 
 def prepare_data(directory=None):
     path = [os.getcwd()]
@@ -69,7 +71,6 @@ def train_model(model, images, datsets, criterion, optimizer, scheduler, num_epo
             # Iterate over data.
 
             for inputs, labels in datasets[phase]:
-                logging.debug(labels)
                 inputs = inputs.to(DEVICE)
                 labels = labels.to(DEVICE)
 
@@ -87,14 +88,14 @@ def train_model(model, images, datsets, criterion, optimizer, scheduler, num_epo
                     if phase == 'train':
                         loss.backward()
 
-                # Sttempting to add Tensorboard logging her
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
 
+                # Sttempting to add Tensorboard logging her
                 optimizer.step()
                 if log:
                     step_count += 1
-
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
+                    tensorboard_logging(step_count, model, inputs, running_loss, running_corrects.double())
 
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects.double() / dataset_sizes[phase]
@@ -118,6 +119,27 @@ def train_model(model, images, datsets, criterion, optimizer, scheduler, num_epo
     return model
 
 
+def tensorboard_logging(step=None, model=None, images=None, loss=None, accuracy=None):
+    """Write logs for this step so we can see stae via Tensorboard"""
+    # 1. Log scalar values (scalar summary)
+    info = { 'loss': loss, 'accuracy': accuracy }
+
+    for tag, value in info.items():
+        LOGGER.scalar_summary(tag, value, step+1)
+
+    # 2. Log values and gradients of the parameters (histogram summary)
+    for tag, value in model.named_parameters():
+        tag = tag.replace('.', '/')
+        LOGGER.histo_summary(tag, value.data.cpu().numpy(), step+1)
+        LOGGER.histo_summary(tag+'/grad', value.grad.data.cpu().numpy(), step+1)
+
+    # 3. Log training images (image summary)
+    info = { 'images': images.view(-1, 28, 28)[:10].cpu().numpy() }
+
+    for tag, images in info.items():
+        LOGGER.image_summary(tag, images, step+1)
+
+
 def initialise_model(images):
     """Create an empty model - TODO specify num_classes from datset
     models.vgg11 =
@@ -139,7 +161,7 @@ def initialise_model(images):
     return model
 
 
-def build_model(images, datasets, epochs=EPOCHS):
+def build_model(images, datasets, epochs=EPOCHS, log=False):
     """Run the training regime on the model and save its best effort"""
     model_ft = initialise_model(images)
     criterion = nn.CrossEntropyLoss()
@@ -148,8 +170,7 @@ def build_model(images, datasets, epochs=EPOCHS):
     # Decay LR by a factor of 0.1 every 7 epochs
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
-    model_ft = train_model(model_ft, images, datasets, criterion, optimizer_ft, exp_lr_scheduler,
-                       num_epochs=epochs)
+    model_ft = train_model(model_ft, images, datasets, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=epochs, log=log)
 
     torch.save(model_ft, os.path.join(os.getcwd(),'model'))
 
@@ -161,8 +182,14 @@ if __name__ == '__main__':
     parser.add_argument(
         '--directory',
         help="Path of a directory on this host")
+    parser.add_argument(
+        '--log',
+        action='store_true')
 
     args = parser.parse_args()
+
+    if args.log:
+        print('logging on')
     images, datasets = prepare_data(directory=args.directory)
-    build_model(images, datasets)
+    build_model(images, datasets, log=args.log)
 
